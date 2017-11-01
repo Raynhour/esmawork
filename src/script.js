d7aae8a713890356843e68b62afcfbcede658b2e
Raynhour <waqdet@gmail.com>
2017-11-01 11:04:25 +0200

    bark

--- src/script.js
+++ src/script.js
@@ -1,8 +1,27 @@
-let person = {
-	firstName: "john",
-	lastName: "will"
-};
+class Dog {
+	constructor(){
+		this._bark = "gavgavgav";
+	}
+	bark () {
+		console.log(this._bark);
+	}
+	
+}
 
-let {firstName, lastName} = person;
+class SubDog extends Dog {
+	constructor(parent) {
+		super();
+		this.parent = parent;
+	}
+	bark () {
+		this._bark = "gavmewgavmew";
+	}
+}
 
-console.log(firstName,lastName);
+scream.bark();
+
+const scream = new Dog();
+const subscream = new SubDog(scream);
+
+scream.bark();
+subscream.bark();
